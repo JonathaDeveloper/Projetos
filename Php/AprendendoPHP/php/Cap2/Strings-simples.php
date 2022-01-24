b5ab04b762457cae384b07adfa2693e79f975d68
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-24 16:05:40 -0300

    Create Strings-simples.php

---
+++ Php/AprendendoPHP/php/Cap2/Strings-simples.php
@@ -0,0 +1,22 @@
+<?php
+
+# Capitulo 2 - Dados: trabalhando com texto e números
+
+// Obs: vou está utilizando sempre o :  . "\r\n"; apenas para ficar mais legivel , é minha forma de está fazendo quebra de linhas
+
+//Utilizando uma string de forma simples
+
+//Exemplo 1 :
+print 'Olá mundo!' . "\t\r\n";
+
+//Exemplo 2:
+print '123' . "\r\n";
+
+//Exemplo 3:
+print 'Quem é "MASTER" ?' ."\r\n";
+
+//Exemplo 4:
+print  'Utilize \' aspas simples com barra invertida antes da aspa \''  . "\r\n";
+ 
+
+?>
