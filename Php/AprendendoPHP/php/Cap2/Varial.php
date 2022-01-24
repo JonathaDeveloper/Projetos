d936d2b896c6cd40fac84da6b3d932ae598a1f77
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-24 16:05:42 -0300

    Create Varial.php

---
+++ Php/AprendendoPHP/php/Cap2/Varial.php
@@ -0,0 +1,18 @@
+<?php
+
+# Capitulo 2 - Dados: trabalhando com texto e números
+
+// Obs: vou está utilizando sempre o :  . "\r\n"; apenas para ficar mais legivel , é minha forma de está fazendo quebra de linhas
+
+// Para declarar uma variavel basta iniciar com $
+
+// Exemplo 1:
+
+$nome = 'Jonatha';
+
+// Obs: podemos tambem imprimir com o comando echo();
+
+echo($nome);
+
+
+?>
