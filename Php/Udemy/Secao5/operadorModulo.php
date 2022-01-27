918915784e6b9df5734270962bfde95f95d7f52b
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-27 11:14:23 -0300

    Create operadorModulo.php

---
+++ Php/Udemy/Secao5/operadorModulo.php
@@ -0,0 +1,16 @@
+<?php
+
+// 
+echo "\n Módulo (Resto)  % <br> \n";
+echo 12 % 2;
+echo "\n Módulo (Divisao) / <br> \n";
+echo 12 / 2;
+//
+echo "\n Módulo (Resto)  % <br> \n";
+echo 7 % 2;
+echo "\n Módulo (Divisao) / <br> \n";
+echo 7 / 2;
+
+
+
+?>
