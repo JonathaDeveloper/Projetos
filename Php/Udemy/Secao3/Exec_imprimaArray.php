1385bf7e273ded45de7c25e7ff581639f0512c28
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-26 22:35:00 -0300

    Create Exec_imprimaArray.php

---
+++ Php/Udemy/Secao3/Exec_imprimaArray.php
@@ -0,0 +1,15 @@
+<?php
+
+// Crie um array com caracteristicas de um carro;
+
+$carro = array();
+
+$carro[] = "Rodas";
+$carro[] = "Motor";
+$carro[] = "Bancos";
+$carro[] = "Parabrisa";
+
+echo($carro[0]);
+echo($carro[3]);
+
+?>
