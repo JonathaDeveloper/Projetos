17602ede627ad24c4a8dd14c88066f6850b0162b
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-26 22:35:09 -0300

    Create is_bool.php

---
+++ Php/Udemy/Secao3/is_bool.php
@@ -0,0 +1,21 @@
+<?php
+
+$pop = array();
+
+
+if(is_bool($pop)){
+    print "$pop é Verdade!";
+} else {
+    print "$pop é Falso!";
+}
+
+$pop2 = true;
+
+if(is_bool($pop2)){
+    print "$pop2 é Verdade!";
+} else {
+    print "$pop2 é Falso!";
+}
+
+
+?>
