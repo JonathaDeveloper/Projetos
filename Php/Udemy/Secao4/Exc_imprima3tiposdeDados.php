b4e4e452ce27590f499bc512c1c8689e1887e37e
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-27 08:34:08 -0300

    Create Exc_imprima3tiposdeDados.php

---
+++ Php/Udemy/Secao4/Exc_imprima3tiposdeDados.php
@@ -0,0 +1,15 @@
+<?php
+
+// Obs: KK Camel Case (você inicia uma variavel como minuscula
+// e a segunda parte você inicia com maiuscula ex:
+// moto top = $motoTop)
+
+$velocidadeMaxima = 300; // int
+$marca = "Fiat"; // String
+$acessorios = ["Guarda Copos", "Porta malas extendido", "flb"]; // array
+
+print "$marca" . "\n <br/>";
+print "$velocidadeMaxima" . "\n <br/>";
+print_r($acessorios) . "\n <br/>";
+
+?>
