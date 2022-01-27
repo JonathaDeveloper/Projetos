4a314d31ecbc484ff8adfa565f64ae4720c2962d
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-27 11:14:29 -0300

    Create operadorTernario.php

---
+++ Php/Udemy/Secao5/operadorTernario.php
@@ -0,0 +1,12 @@
+<?php
+
+// o 1º ? é "Caso essa operação dê : true"
+// o 2º : é "Caso essa opereção dÊ : false"
+
+echo 20 > 10 ? "\n Deu certo é Verdade ! \n <br/>" : "\n Deu erro é Falso ! \n <br/>";
+
+echo 20 > 100 ? "\n Deu certo é Verdade ! \n <br/>" : "\n Deu erro é Falso ! \n <br/>";
+
+echo !(20 > 100) ? "\n Deu certo é Verdade ! \n <br/>" : "\n Deu erro é Falso ! \n <br/>";
+
+?>
