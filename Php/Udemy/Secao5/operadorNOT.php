98b41d58ed110dcb5531c6f0c01e118f1e037ea4
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-27 11:14:27 -0300

    Create operadorNOT.php

---
+++ Php/Udemy/Secao5/operadorNOT.php
@@ -0,0 +1,12 @@
+<?php
+
+// Sem uso na negacao
+if(5 > 2){
+    print "Validação Verdade \n";
+} else print "Validação Falsa \n";
+
+// Com uso na negacao
+if(!(20 > 5)){
+    print "Validação Verdade \n";
+} else print "Validação Falsa \n";
+?>
