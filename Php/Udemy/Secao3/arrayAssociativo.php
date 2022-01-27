917fb8df8e83c5b22a66fa27cf97e27b7e23884a
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-26 22:34:58 -0300

    Create arrayAssociativo.php

---
+++ Php/Udemy/Secao3/arrayAssociativo.php
@@ -0,0 +1,13 @@
+<?php
+
+$lista = ['chave' => 'valor',
+          10 => 'Joao',
+          20 => 'Maria'];
+          
+echo($lista['chave']);
+echo "\n <br/>";
+echo($lista[10]);
+echo "\n <br/>";
+echo($lista[20]);
+
+?>
