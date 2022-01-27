3a1d8335997b3c27d684d3955a6ebef101731e3f
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-27 08:34:12 -0300

    Create variavelDinamica_var_to_var.php

---
+++ Php/Udemy/Secao4/variavelDinamica_var_to_var.php
@@ -0,0 +1,14 @@
+<?php
+
+// Pega referencia de outra variaval
+
+
+$x = "nome"; // a variavel (X) = nome
+
+print "$x <br/> \n";
+
+$$x = "nome2"; // o valor de (x)  é o nome que pode ser uma variavel, com o valor de (nome2)
+
+print "$nome <br/> \n";
+
+?>
