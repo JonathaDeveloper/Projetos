1b4c431e2986ed8625a23bfe701b4e4041f1b4c6
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-26 22:34:56 -0300

    Create Exercicio_caracteristicas.php

---
+++ Php/Udemy/Secao2/Exercicio_caracteristicas.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+
+Descreva as caracteristicas de uma pessoa ou objeto e descreva as mesmas utilizando comentarios
+
+*/
+
+// Nome da pessoa
+
+$nome = 'JONATHA';
+
+// Sobrenome da pessoa
+
+$sobrenome = 'ARAUJO OLIVEIRA';
+
+
+// Endereço
+
+$end = 'Rua X , Bairro São Francisco';
+
+// Mostrando o endereço
+
+print "O Sr. $nome $sobrenome , mora no endereço : $end \n";
+
+
+?>
