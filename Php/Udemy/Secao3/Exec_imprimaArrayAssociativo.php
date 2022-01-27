d40b0a4b6f06d7ec9216135fc61ae9f8fd9ff744
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-26 22:35:06 -0300

    Create Exec_imprimaArrayAssociativo.php

---
+++ Php/Udemy/Secao3/Exec_imprimaArrayAssociativo.php
@@ -0,0 +1,16 @@
+<?php
+
+$cliente = array('id' => 1,
+                'Nome' => 'Jonatha',
+                'Idade' => 29,
+                'Profissao' => 'Gerente Implantacao');
+echo "\n <br/>";
+echo($cliente['id']);
+echo "\n <br/>";
+echo($cliente['Nome']);
+echo "\n <br/>";
+echo($cliente['Idade']);
+echo "\n <br/>";
+echo($cliente['Profissao']);
+
+?>
