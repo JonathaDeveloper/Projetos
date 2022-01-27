604a016db67607d237d12760ed866abdc4c73666
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-26 22:35:12 -0300

    Create is_float.php

---
+++ Php/Udemy/Secao3/is_float.php
@@ -0,0 +1,11 @@
+<?php
+// funcao is_float() serve para validar se um determinado dado é ponto flutuante
+
+// A expressao hard cod é quando eu digito um valor na mão conforme o exemplo a baixo onde digitei 5.5 em vez de buscar de uma variavel
+
+if(is_float(5.5)){
+ print "É float!";
+} else print "Não é float";
+
+
+?>
