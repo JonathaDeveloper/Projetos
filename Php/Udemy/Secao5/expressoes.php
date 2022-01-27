951188eebff67aec161838778b11121db8c55dea
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-27 11:14:22 -0300

    Create expressoes.php

---
+++ Php/Udemy/Secao5/expressoes.php
@@ -0,0 +1,19 @@
+<?php
+
+// Inteiro
+
+$n1 = "5" + 12;
+
+echo("\n Resultado : $n1 \n");
+
+echo(gettype($n1));
+
+// Double
+
+$n1 = "5.2" + 12;
+
+echo("\n Resultado : $n1 \n");
+
+echo(gettype($n1));
+
+?>
