aca2463365c986af334e4e1ff628fd3c0e470e62
Jonatha Araujo Oliveira <jonatha.developer@outlook.com>
2022-01-28 20:35:52 -0300

    Create array.php

---
+++ Php/AprendendoPHP/php/Cap4/array.php
@@ -0,0 +1,16 @@
+<?php
+
+//$variavel = array( chave => valor);
+
+$vegetais = array(
+    'brocolis' => 'verde',
+    'beterraba' => 'roxo',
+    'cenoura' => 'amarelo',
+    'alfase' => 'verde'
+);
+
+$cidades = array(
+    0 => 'Sao Paulo',
+    1 => 'Rio de Janeiro',
+    2 => 'Brasilia'
+);
