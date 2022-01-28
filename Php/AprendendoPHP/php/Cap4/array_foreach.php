46e6199c1f3f79da5d32d1c296865e1cf276857a
Jonatha Araujo Oliveira <jonatha.developer@outlook.com>
2022-01-28 20:36:02 -0300

    Create array_foreach.php

---
+++ Php/AprendendoPHP/php/Cap4/array_foreach.php
@@ -0,0 +1,6 @@
+<?php
+// percorrer o array com foreach()
+
+foreach($listaProdutos as $lista => $produtos){ // estou definindo key ($lista), value ($produtos)
+    print("$lista - $produtos" . "\n");
+}
