3e2d2886d868b8590c3be592ea06de898e391678
Jonatha Araujo Oliveira <jonatha.developer@outlook.com>
2022-01-29 07:58:27 -0300

    Create Strings_simples.php

---
+++ Php/AprendendoPHP/php/Cap2/Strings_simples.php
@@ -0,0 +1,29 @@
+<?php
+
+# Capitulo 2 - Dados: trabalhando com texto e números
+
+// Obs: Vou está utilizando sempre o :  . "<br/>"; apenas para ficar mais legivel , é minha forma de está fazendo quebra de linhas
+// Obs2: Caso esteja utilizando a execução no navegador utilize <br/> caso seja no console ou terminal utlize /r/n
+
+//Utilizando uma string de forma simples
+
+//Exemplo 1 :
+print 'Olá mundo!' . "<br/>";
+
+//Exemplo 2:
+print '123' . "<br/>";
+
+//Exemplo 3:
+print 'Quem é "MASTER" ?' . "<br/>";
+
+//Exemplo 4:
+print  'Utilize \' aspas simples com barra invertida antes da aspa \'' . "<br/>";
+
+//Exemplo 5:
+print 'Utilize duas barras \\ para ver uma barra na impressão' . "<br/>";
+ 
+//Exemplo 6:
+echo('Tambem posso utilizar o comando echo() com aspas simples para imprimir' . "<br/>");
+
+
+?>
