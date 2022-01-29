a2c0f1a9db8a283fb82c094d501381526b8af554
Jonatha Araujo Oliveira <jonatha.developer@outlook.com>
2022-01-29 20:52:17 -0300

    Update class_metodo_statico.php

--- Php/AprendendoPHP/php/Cap6/class_metodo_statico.php
+++ Php/AprendendoPHP/php/Cap6/class_metodo_statico.php
@@ -1,18 +1,47 @@
 <?php
 
-class Cadastros3 {
-    public string $nome;
-    public int $cpf;
-    public int $n1 = 5;
-    public int $n2 = 5;
 
-    public static function funcaoEstatica(){
-        $n1 = 10;
-        $n2 = 30;
-        $soma = $n1 + $n2;
-        return $soma;
+
+class PrecoProduto {
+    public int $cod_prod;
+    public string $descricao;
+    public int $qtd;
+    public float $custo_compra;
+
+/*
+Metodos estaticos não podem utilizar a variavel $this, já que não são
+executados no contexto de uma instância de objeto especifico e sim na propria classe,
+São importantes para finalidade dentro da classe e não para o objeto 
+*/
+
+    public static function parametro_margemLucro(){
+        $margem = 0.25;
+        return $margem;
     }
-}
-$cadastroEstatico = Cadastros3::funcaoEstatica();
 
-echo($cadastroEstatico);
+    function cadastraProduto($cod_prod, $custo_compra, $descricao, $qtd){
+        $this->cod_prod      = $cod_prod;
+        $this->custo_compra  = $custo_compra;
+        $this->descricao     = $descricao;
+        $this->qtd           = $qtd;
+
+        return "\n
+                {$this->cod_prod} \n
+                {$this->custo_compra} \n
+                {$this->descricao} \n
+                {$this->qtd} \n
+                \n";
+    }
+
+}
+$cl_Estatico = PrecoProduto::parametro_margemLucro();
+
+echo($cl_Estatico);
+
+$cadastraItem = new PrecoProduto;
+
+echo($cadastraItem->cadastraProduto(1,100,'Mouse',100));
+
+$cadastraItem->custo_compra += ($cl_Estatico * 100);
+
+echo($cadastraItem->custo_compra);
