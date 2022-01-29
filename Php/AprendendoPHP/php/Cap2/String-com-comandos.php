e37792507c83334ebccca9584ad3c35385a7ab56
Jonatha Araujo Oliveira <jonatha.developer@outlook.com>
2022-01-29 07:58:23 -0300

    Create String-com-comandos.php

---
+++ Php/AprendendoPHP/php/Cap2/String-com-comandos.php
@@ -0,0 +1,26 @@
+<?php
+
+# Capitulo 2 - Dados: trabalhando com texto e números
+
+// Obs: Vou está utilizando sempre o :  . "<br/>"; apenas para ficar mais legivel , é minha forma de está fazendo quebra de linhas
+// Obs2: Caso esteja utilizando a execução no navegador utilize <br/> caso seja no console ou terminal utlize /r/n
+
+
+// Utilizando aspas duplas temos melhor controle sobre o que vamos exibir ou seja podemos mostrar o valor de uma varial dentro da string chamando a mesma
+
+$n1 = 10;
+$n2 = 20;
+
+$total = $n1+$n2;
+
+echo("$n1 + $n2 = $total \n" . "<br/>");
+
+// Podemos concatenar "juntar duas partes"
+
+$nome = "Jonatha";
+$sobrenome = "Oliveira";
+
+print " $nome $sobrenome \n" . "<br/>";
+
+
+?>
