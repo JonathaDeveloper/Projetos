296f13aedd6b0b5b022cdfe123ebd7295d532fc5
JonathaDeveloper <jonatha.developer@outlook.com>
2022-01-30 22:44:53 -0300

    Create switch.php

---
+++ Php/Udemy/Secao6/switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$x = 20;
+
+
+switch($x) {
+    case 0:
+        echo "X é igual a 0 <br/> \n";
+    break;
+    case 1:
+        echo "X é igual a 1 <br/> \n";
+    break; 
+    default:
+        echo "X não é igual a nenhum valor mencionado \n ";
+}
+
+?>
